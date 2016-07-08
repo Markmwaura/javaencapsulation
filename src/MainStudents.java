8fd6bc0823300f0d76a765bb1966238702b0eb33
Mark Mwaura <markkmwaura@gmail.com>
2016-07-08 05:26:27 +0300

    added java project

---
+++ src/MainStudents.java
@@ -0,0 +1,16 @@
+
+public class MainStudents {
+
+	public static void main(String[] args) {
+		Student std = new Student();
+		std.setName("Mike");
+		std.setAge(23);
+		std.setIdNum("89872");
+		
+	System.out.println("Name:"+std.getName()+" Age :"+std.getAge());
+
+		
+
+	}
+
+}
---
+++ src/Student.java
@@ -0,0 +1,47 @@
+
+public class Student {
+	
+	private String name;
+	   private String idNum;
+	   private int age;
+	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the idNum
+	 */
+	public String getIdNum() {
+		return idNum;
+	}
+	/**
+	 * @param idNum the idNum to set
+	 */
+	public void setIdNum(String idNum) {
+		this.idNum = idNum;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
